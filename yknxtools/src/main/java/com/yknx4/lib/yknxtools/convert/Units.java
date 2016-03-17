6a6aee1737fd020c15723236300b107e560e9cd8
Jorge Figueroa <yknx.4.b@gmail.com>
2016-03-16 20:50:28 -0600

    Add unit utility

---
+++ yknxtools/src/main/java/com/yknx4/lib/yknxtools/convert/Units.java
@@ -0,0 +1,24 @@
+package com.yknx4.lib.yknxtools.convert;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.TypedValue;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by yknx4 on 3/16/16.
+ */
+public class Units {
+    private static Map<Integer, Float> pxForDp = new HashMap<>();
+    public static float getPixelsForDp(Context context, int dp){
+        if(pxForDp.containsKey(dp))
+            return pxForDp.get(dp);
+        Resources r = context.getResources();
+        float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics());
+        pxForDp.put(dp, px);
+        return px;
+    }
+
+}
