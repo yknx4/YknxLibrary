f4ed4bb351047ecf03d2ba583007891c41c51518
Jorge Figueroa <jorge.figueroa@magmalabs.io>
2016-03-31 16:35:40 -0600

    get string from stream

---
+++ yknxtools/src/main/java/com/yknx4/lib/yknxtools/DateTools.java
@@ -0,0 +1,13 @@
+package com.yknx4.lib.yknxtools;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+/**
+ * Created by yknx4 on 3/31/16.
+ */
+public class DateTools {
+
+    public static final SimpleDateFormat railsStandardDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS", Locale.US);
+
+}
